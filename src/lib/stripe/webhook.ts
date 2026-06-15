42968a07ad2f70457e877ce3befc22f8cd875a45
johnmwathi23 <johnmwathi23@gmail.com>
2026-06-16 01:26:27 +0300

    Fix webhook.ts to use new stripe() function and getWebhookSecret()

--- src/lib/stripe/webhook.ts
+++ src/lib/stripe/webhook.ts
@@ -1,6 +1,6 @@
 import Stripe from "stripe"
-import { stripe, STRIPE_WEBHOOK_SECRET } from "./client"
+import { stripe, getWebhookSecret } from "./client"
 
 export function constructEvent(body: string, signature: string): Stripe.Event {
-  return stripe.webhooks.constructEvent(body, signature, STRIPE_WEBHOOK_SECRET)
+  return stripe().webhooks.constructEvent(body, signature, getWebhookSecret())
 }
